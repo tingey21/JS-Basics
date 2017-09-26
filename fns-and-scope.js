2370d1297f6ae09ca9a1915a075e51ddaad6f7cf
tingey21 <alextingey1@gmail.com>
2017-09-26 16:11:43 -0600

    complete

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,7 +4,13 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+ function isTyler(maybe){
+ 	if (maybe ==='Tyler') {
+ 		return true;
+ 	}else{
+ 		return false;
+ 	}
+ }
 
 //////////////////PROBLEM 2////////////////////
 
@@ -12,7 +18,11 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+ function getName(){
+ 	var person = prompt("Please enter your name: ");
+ 	return person;
+
+ }
 
 
 //////////////////PROBLEM 3////////////////////
@@ -22,7 +32,10 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+ function welcome(){
+ var name = getName();
+ 	alert( "Welcome, " + name);
+ }
 
 
 //////////////////PROBLEM 4////////////////////
@@ -32,8 +45,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
-
+//parameters are open ended and you dont need an arguement 
 
 //////////////////PROBLEM 5////////////////////
 
@@ -42,7 +54,13 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  //
+   // false, zero and empty strings are all equivalent.
+   // null and undefined are equivalent to themselves and each other but nothing else.
+  //  NaN is not equivalent to anything – including another NaN!
+  //  Infinity is truthy – but cannot be compared to true or false!
+   // An empty array is truthy – yet comparing with true is false and comparing with false is true?!
+
 
 
 
@@ -52,16 +70,16 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
-  
+function myName() {
+	return "alex";
+}
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
-
+var newMyName = myName;
 //Now alert the result of invoking newMyName
-
+alert(newMyName);
 
 
 //////////////////PROBLEM 7////////////////////
@@ -70,10 +88,14 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
-
+  function outerFn(){
+  	return function(){
+  		return "alex";
+  	}
+  }
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+  var innerFn = outerFn();
 
 //Now invoke innerFn.
+innerFn;
